3272e92aa8345e10dc5166074840951a438312fa
marcelo <marcelogracia@hotmail.com>
2015-06-03 00:04:49 -0300

    agregue administracion de los modelos que están dentro de la app bases. Defini modelos, quizas hay que ver si se reflejan mas configuraciones a los modelos en el administrador, pero como son tablas bases, creo que no son necesarias

--- weredone/bases/admin.py
+++ weredone/bases/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import TipoUsuario,TipoObjeto,TipoObjetivo,TipoComentario,TipoInconveniente
 
 # Register your models here.
+@admin.register(TipoUsuario,TipoObjeto,TipoObjetivo,TipoComentario,TipoInconveniente)
+class TipoUsuarioAdmin(admin.ModelAdmin):
+    pass
+
+class TipoObjetoAdmin(admin.ModelAdmin):
+	pass
+
+class TipoObjetivoAdmin(admin.ModelAdmin):
+	pass
+
+class TipoComentarioAdmin(admin.ModelAdmin):
+	pass
+
+class TipoInconvenienteAdmin(admin.ModelAdmin):
+	pass
--- weredone/bases/models.py
+++ weredone/bases/models.py
@@ -2,5 +2,36 @@ from django.db import models
 
 # Create your models here.
 class TipoUsuario(models.Model):
-  Desciption = models.CharField(max_length=100)
+  Description = models.CharField(max_length=100)
   Status = models.BooleanField
+
+  def __unicode__(self):
+		return "%s" % (self.Description)
+
+class TipoObjeto(models.Model):
+  Description = models.CharField(max_length=100)
+  Status = models.BooleanField
+
+  def __unicode__(self):
+		return "%s" % (self.Description)
+
+class TipoObjetivo(models.Model):
+  Description = models.CharField(max_length=25)
+  Status = models.BooleanField
+
+  def __unicode__(self):
+		return "%s" % (self.Description)
+
+class TipoComentario(models.Model):
+  Description = models.CharField(max_length=25)
+  Status = models.BooleanField
+
+  def __unicode__(self):
+		return "%s" % (self.Description)
+
+class TipoInconveniente(models.Model):
+  Description = models.CharField(max_length=25)
+  Status = models.BooleanField
+
+  def __unicode__(self):
+		return "%s" % (self.Description)
