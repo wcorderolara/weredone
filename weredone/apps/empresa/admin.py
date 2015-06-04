aa738aebc31f5e83160b1147d5d3a274cfdcc583
marcelo <marcelogracia@hotmail.com>
2015-06-03 23:56:45 -0300

    agregue modelo para administrar empresas

--- weredone/apps/empresa/admin.py
+++ weredone/apps/empresa/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Empresa
 
-# Register your models here.
+
+@admin.register(Empresa)
+class EmpresaAdmin(admin.ModelAdmin):
+    pass
--- weredone/apps/empresa/models.py
+++ weredone/apps/empresa/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Empresa(models.Model):
+    Name = models.CharField(max_length=100)
+    Email = models.EmailField(blank=False)
+
+    def __unicode__(self):
+        return "%s" % (self.Name)
