f6e3ea0d38fe1cdb638ba518fd61a46532d4fb84
marcelo <marcelogracia@hotmail.com>
2015-06-11 23:02:11 -0300

    varios reordenamientos y agregado de my.cnf para conexion en bbdd de desarrollo local

--- weredone/weredone/settings.py
+++ weredone/weredone/settings.py
@@ -82,8 +82,10 @@ WSGI_APPLICATION = 'weredone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'my.cnf'),
+        }
     }
 }
 
